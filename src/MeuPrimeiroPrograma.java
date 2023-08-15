ef26b433062eb016edea6dbd24ffa4c0946373b5
thiago-braga <thiago.braga@wyntech.inf.br>
2023-08-14 23:24:20 -0300

    adicionado novos métodos de calculo

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -7,6 +7,10 @@ public class MeuPrimeiroPrograma {
             sum(x,y);
         }else if(args[0].equals("subtrair")) {
             minus(x,y);
+        }else if (args[0].equals("dividir")) {
+            divide(x,y);
+        }else if (args[0].equals("multiplicar")) {
+            multiple(x,y);
         }
     }
 
@@ -17,5 +21,14 @@ public class MeuPrimeiroPrograma {
     static void minus(int x, int y) {
         System.out.println(x - y);
     }
+
+    static void divide(int x, int y) {
+        System.out.println(x / y);
+    }
+
+    static void multiple(int x, int y) {
+        System.out.println(x * y);
+    }
+
 }
 
