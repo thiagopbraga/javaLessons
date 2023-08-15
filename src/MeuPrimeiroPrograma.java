f3011a6acdd359c1c80fcf0d9c79cc2890f2075c
thiago-braga <thiago.braga@wyntech.inf.br>
2023-08-14 23:09:07 -0300

    another commit

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,6 +1,21 @@
 public class MeuPrimeiroPrograma {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
 
-        System.out.println(args[0]);
+        if(args[0].equals("somar")) {
+            sum(x,y);
+        }else if(args[0].equals("subtrair")) {
+            minus(x,y);
+        }
+    }
+
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+
+    static void minus(int x, int y) {
+        System.out.println(x - y);
     }
 }
+
